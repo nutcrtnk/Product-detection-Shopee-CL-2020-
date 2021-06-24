058fc6c5b8a5e4b77ba9a45244707ea8e929a949
anon <->
2021-06-25 00:53:23 +0900

    Revert "Upload code"
    This reverts commit 1480901cf101337ed54d3a7b29b8c3777ba94e43.

--- config.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-data_path = Path('../data')
-data_split_path = Path('../data_split')
-log_path = Path('../logs')
-model_path = Path('../models')
-ana_path = Path('../analyze')
-data_text_path = Path('../data_text')
